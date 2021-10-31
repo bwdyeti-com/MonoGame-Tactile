18581315080408e3bcdaaa7c8dacd8d9e41e8145
BwdYeti <BwdYeti@gmail.com>
2021-10-31 18:49:41 -0400

    Drag Start Position
    -Get the start position of drags and store it as Position2 of GestureSamples

--- MonoGame.Framework/Input/Touch/TouchPanelState.cs
+++ MonoGame.Framework/Input/Touch/TouchPanelState.cs
@@ -719,6 +719,7 @@ namespace Microsoft.Xna.Framework.Input.Touch
         }
 
         private GestureType _dragGestureStarted = GestureType.None;
+        private Vector2 _dragGestureStartLoc;
 
         private void ProcessDrag(TouchLocation touch)
         {
@@ -743,6 +744,12 @@ namespace Microsoft.Xna.Framework.Input.Touch
                 var isHorizontalDelta = Math.Abs(delta.X) > Math.Abs(delta.Y * 2.0f);
                 var isVerticalDelta = Math.Abs(delta.Y) > Math.Abs(delta.X * 2.0f);
                 var classify = _dragGestureStarted == GestureType.None;
+
+                // Get the starting position of the drag to store as Position2
+                if (classify)
+                {
+                    _dragGestureStartLoc = prevTouch.Position;
+                }
 
                 // Once we enter either vertical or horizontal drags
                 // we stick to it... regardless of the delta.
@@ -780,7 +787,7 @@ namespace Microsoft.Xna.Framework.Input.Touch
 
             GestureList.Enqueue(new GestureSample(
                                     _dragGestureStarted, touch.Timestamp,
-                                    touch.Position, Vector2.Zero,
+                                    touch.Position, _dragGestureStartLoc,
                                     delta, Vector2.Zero));
         }
 
